9d9c13040d5b351071972c8097a19ef25f19c1f2
jwong65 <jason.wong6507@gmail.com>
2023-01-17 21:28:57 -0500

    Added the button based on the instruction from class

--- 7.1.3-declarative-counter/src/App.js
+++ 7.1.3-declarative-counter/src/App.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react'
 
 function App(){
-  let [counter, changeCounter] = useState(1)
+  let [counter, setCounter] = useState(1)
   //Declarative Rendering
-  window.changeCounter = changeCounter
+  let newCount =() => {setCounter(counter+1)}
+  // window.changeCounter = setCounter
   return(
-  <h1>{counter}</h1>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+      {/* Virtual DOM so changeCounter is needed */}
+      <button onClick={newCount}>Increase the Counter</button>
+    </div>
   )
 }
 
